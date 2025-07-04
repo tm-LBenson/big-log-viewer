052bf13f6e042d8c29cf3ae05a8b75fa924ea6aa
Lewis Benson <tm.lewisbenson@gmail.com>
2025-07-04 16:49:45 -0400

    fix script

--- windows/install_biglog.ps1
+++ windows/install_biglog.ps1
@@ -1,30 +1,37 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-$repoUrl = "https://github.com/tm-LBenson/big-log-viewer.git"
-$appName = "biglog.exe"
+$repoUrl     = "https://github.com/tm-LBenson/big-log-viewer.git"
+$appName     = "biglog.exe"
 $desktopPath = [Environment]::GetFolderPath("Desktop")
+$tempPath    = Join-Path $env:TEMP "biglog_temp"
+
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Error "winget is required but not found. Please install Windows 10 1809+ or use the Microsoft Store version of 'App Installer'."
+    exit 1
+}
 
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
-    Write-Host "Git not found. Installing via winget..."
+    Write-Host "Installing Git..."
     winget install --id Git.Git -e --source winget
 }
 
 if (-not (Get-Command go -ErrorAction SilentlyContinue)) {
-    Write-Host "Go not found. Installing via winget..."
+    Write-Host "Installing Go..."
     winget install --id GoLang.Go -e --source winget
 }
 
-$tempPath = "$env:TEMP\biglog_temp"
 if (Test-Path $tempPath) { Remove-Item -Recurse -Force $tempPath }
-git clone $repoUrl $tempPath
+git clone --depth 1 $repoUrl $tempPath
 
 Push-Location $tempPath
-go build -o $appName
+go build -o $appName ./cmd/biglog
 Pop-Location
 
-Move-Item "$tempPath\$appName" "$desktopPath\$appName" -Force
+Move-Item -Path (Join-Path $tempPath $appName) `
+          -Destination (Join-Path $desktopPath $appName) -Force
 
 Remove-Item -Recurse -Force $tempPath
 
-Write-Host "$appName has been built and moved to your Desktop."
+Write-Host "`n Built and placed '$appName' on your Desktop."
+Write-Host "   Run with: `"$desktopPath\$appName`" -logdir `"C:\path\to\logs`""
